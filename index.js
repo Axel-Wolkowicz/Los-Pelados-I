642b2ccb4e0d9beda45d1cf703f40dddbcff0230
Axel-Wolkowicz <wolkowiczaxel@gmail.com>
2025-09-18 12:08:36 -0300

    termine base

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ import {
   seleccionarSabores,
 } from "./dataEntry.js";
 
+import fs from "fs";
+
 // Cargar sabores y productos
 // COMPLETEN USTEDES
-let sabores;
-let productos;
+let sabores = fs.readFileSync("data/sabores.json", "utf-8");
+sabores = JSON.parse(sabores);
+let productos = fs.readFileSync("data/productos.json", "utf-8");
+productos = JSON.parse(productos);
 
 // Ingresar cliente
 let cliente = ingresarCliente();
@@ -18,6 +22,13 @@ let producto = seleccionarProducto(productos);
 let gustos = seleccionarCantidadGustos(producto.maxGustos);
 // Elegir sabores
 let saboresElegidos = seleccionarSabores(sabores, gustos);
-
 // Guardar pedido
 // COMPLETEN USTEDES
+let nuevoPedido ={
+  cliente: cliente,
+  producto: producto,
+  sabores: saboresElegidos
+}
+let pedidos = JSON.parse(fs.readFileSync("data/pedidos.json", "utf-8"));
+pedidos.push(nuevoPedido);
+fs.writeFileSync("data/pedidos.json", JSON.stringify(pedidos, null, 2), "utf-8");
